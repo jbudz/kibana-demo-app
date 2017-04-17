909185a08e7abfe7e1d3fd55b7e2aa9f2eee9ae8
Jonathan Budzenski <jon@jbudz.me>
2017-04-17 13:12:10 -0500

    Cleanup

--- index.js
+++ index.js
@@ -4,30 +4,20 @@ import status from './server/routes/status';
 export default function (kibana) {
   return new kibana.Plugin({
     require: ['elasticsearch'],
-
     uiExports: {
-
       app: {
         title: 'My Plugin',
         description: 'An awesome Kibana plugin',
         main: 'plugins/my_plugin/app'
-      },
-      translations: [
-        resolve(__dirname, './translations/es.json')
-      ]
+      }
     },
-
     config(Joi) {
       return Joi.object({
         enabled: Joi.boolean().default(true),
       }).default();
     },
-
-
     init(server, options) {
       status(server);
     }
-
-
   });
 };
